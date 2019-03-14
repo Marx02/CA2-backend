36e6f6d69137bd75de803df6e2a30066d3ebb8e0
Marx02 <casper_marx@msn.com>
2019-03-14 11:47:34 +0100

    HobbyREST begun

--- CA2-backend/src/main/java/rest/HobbyFacadeREST.java
+++ CA2-backend/src/main/java/rest/HobbyFacadeREST.java
@@ -5,10 +5,14 @@
  */
 package rest;
 
+import com.google.gson.Gson;
+import data.HobbyFacade;
 import entity.Hobby;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
@@ -19,20 +23,37 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 /**
  *
  * @author caspe
  */
 @Stateless
-@Path("entity.hobby")
+@Path("hobby")
 public class HobbyFacadeREST {
 
-    @PersistenceContext(unitName = "CA2DB")
-    private EntityManager em;
+    Gson gson = new Gson();
+
+    HobbyFacade hf = new HobbyFacade();
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("CA2DB");
 
     public HobbyFacadeREST() {
 
+    }
+
+    public HobbyFacadeREST(EntityManagerFactory emf) {
+
+    }
+
+    @GET
+    @Path("/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllHobbies() {
+
+        //  PersonFullDTO pdto = new PersonFullDTO(p);
+        return Response.ok().entity(gson.toJson(hf.getAllHobbys())).build();
     }
 
 }
