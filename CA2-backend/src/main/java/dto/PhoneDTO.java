3f18ab6677a3dd577fc2e914d43540bc564c94b5
Marx02 <casper_marx@msn.com>
2019-03-14 09:52:40 +0100

    Phone dto added. PhoneFacade should be done

---
+++ CA2-backend/src/main/java/dto/PhoneDTO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dto;
+
+/**
+ *
+ * @author caspe
+ */
+public class PhoneDTO {
+    
+    int number;
+    String description;
+
+    public PhoneDTO(int number, String description) {
+        this.number = number;
+        this.description = description;
+    }
+    
+    
+    
+}
